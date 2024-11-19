86c55e38d40e61764d5f8c066220105e624c7863
Hugo Kermabon <hugo.kermabonbobinnec@concordia.ca>
2024-11-19 17:49:24 -0500

    pushing qemu

---
+++ source/qemu/build/libcommon.a.p/common-user_safe-syscall.S.o.d
@@ -0,0 +1,3 @@
+libcommon.a.p/common-user_safe-syscall.S.o: ../common-user/safe-syscall.S \
+ /usr/include/stdc-predef.h ../linux-user/include/special-errno.h \
+ ../common-user/host/x86_64/safe-syscall.inc.S
